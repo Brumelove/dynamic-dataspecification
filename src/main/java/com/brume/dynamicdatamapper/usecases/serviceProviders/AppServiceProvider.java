e71a949b68148ff33726e47c8bb9844eb501dbf5
brumelove <brumelove1996@gmail.com>
2020-04-25 02:19:08 +0100

    fixing bugs

--- src/main/java/com/brume/dynamicdatamapper/usecases/serviceProviders/AppServiceProvider.java
+++ src/main/java/com/brume/dynamicdatamapper/usecases/serviceProviders/AppServiceProvider.java
@@ -23,128 +23,131 @@ public class AppServiceProvider {
 
     private final IEntryRepository entryRepository;
 
-
     @Autowired
-    public AppServiceProvider(IAttributeRepository attributeRepository, IProviderRepository providerRepository, IEntryRepository entryRepository) {
+    public AppServiceProvider(IAttributeRepository attributeRepository, IProviderRepository providerRepository,
+                              IEntryRepository entryRepository) {
         this.attributeRepository = attributeRepository;
         this.providerRepository = providerRepository;
         this.entryRepository = entryRepository;
     }
 
     public ExecutionMethodResponse createDataSpecification(Long providerId, List<String> fields) {
-        if (fields.size () == 0) {
-            return new ExecutionMethodResponse ( false, "fields definition can't be empty" );
+        if (fields.size() == 0) {
+            return new ExecutionMethodResponse(false, "fields definition can't be empty");
         }
 
-        val provider = new Provider ();
-        provider.setId( providerId );
-        provider.setFields ( String.join ( ",", fields ) );
+        val provider = new Provider();
+        provider.setId(providerId);
+        provider.setFields(String.join(",", fields));
 
-        providerRepository.save ( provider );
+        providerRepository.save(provider);
 
-        return new ExecutionMethodResponse ( true, "successfully created provider" );
+        return new ExecutionMethodResponse(true, "successfully created provider");
 
     }
 
     public ExecutionMethodResponse loadData(Long providerId, List<Map<String, Object>> data) {
         // so we'd first check if the provider exists.
-        Optional<Provider> provider = providerRepository.findById ( providerId );
-        if (provider.isPresent ()) {
+        Optional<Provider> provider = providerRepository.findById(providerId);
+        if (provider.isPresent()) {
             // next we validate the field specification.
             Provider gottenProvider = provider.get();
 
-            Set<String> fields = new HashSet<> ( Arrays.asList ( gottenProvider.getFields ().split ( "," ) ) );
-            List<Entry> entriesToSave = new ArrayList<> ();
-            for (int i = 0; i < data.size (); i++) {
-                Entry entry = new Entry ();
-                entry.setProvider ( gottenProvider );
+            Set<String> fields = new HashSet<>(Arrays.asList(gottenProvider.getFields().split(",")));
+            List<Entry> entriesToSave = new ArrayList<>();
+            for (int i = 0; i < data.size(); i++) {
+                Entry entry = new Entry();
+                entry.setProvider(gottenProvider);
 
-                Map<String, Object> singleEntry = data.get ( i );
-                log.info ( "Map data", singleEntry.values () );
+                entryRepository.save(entry);
 
-                Set<String> listOfKeys = new HashSet<> (singleEntry.keySet ());
-                listOfKeys.removeAll ( fields );
+                Map<String, Object> singleEntry = data.get(i);
+                log.info("Map data", singleEntry.values());
 
-                if (listOfKeys.size () > 0) {
-                    return new ExecutionMethodResponse ( false, "Fields " + String.join ( ", ", listOfKeys )
-                                                                + " weren't specified when creating the provider" );
+                Set<String> listOfKeys = new HashSet<>(singleEntry.keySet());
+                listOfKeys.removeAll(fields);
+
+                if (listOfKeys.size() > 0) {
+                    return new ExecutionMethodResponse(false, "Fields " + String.join(", ", listOfKeys)
+                                                              + " weren't specified when creating the provider");
                 }
 
-                List<Attribute> attributesList = new ArrayList<> ();
+                List<Attribute> attributesList = new ArrayList<>();
 
-                for (Map.Entry<String, Object> attributeMap : singleEntry.entrySet ()) {
-                    attributesList.add ( Attribute.fromMapEntry ( entry, attributeMap ) );
+                for (Map.Entry<String, Object> attributeMap : singleEntry.entrySet()) {
+                    Attribute createdAttribute = Attribute.fromMapEntry(entry, attributeMap);
+                    attributesList.add(createdAttribute);
+                    attributeRepository.save(createdAttribute);
                 }
 
-                entry.setAttributes ( attributesList );
+                entry.setAttributes(attributesList);
 
-                entriesToSave.add ( entry );
+                entriesToSave.add(entry);
             }
 
+            //gottenProvider.setEntries(entriesToSave);
 
-            gottenProvider.setEntries ( entriesToSave );
+            //providerRepository.save(gottenProvider);
 
-            providerRepository.save ( gottenProvider );
-
-            return new ExecutionMethodResponse ( true, "" );
+            return new ExecutionMethodResponse(true, "");
 
         }
-        return new ExecutionMethodResponse ( false, "Could not find provider" );
+        return new ExecutionMethodResponse(false, "Could not find provider");
     }
 
     public List<Map<String, Object>> getDataForProvider(Long providerId, Map<String, String> filters) {
-        Optional<Provider> provider = providerRepository.findById ( providerId );
-        if (!provider.isPresent ()) {
+        Optional<Provider> provider = providerRepository.findById(providerId);
+        if (!provider.isPresent()) {
             return null;
         }
 
-        Provider gottenProvider = provider.get ();
+        Provider gottenProvider = provider.get();
 
-        List<Map<String, Object>> results = new ArrayList<> ();
-        Map<Long, Entry> entriesMap = new HashMap<> ();
+        List<Map<String, Object>> results = new ArrayList<>();
+        Map<Long, Entry> entriesMap = new HashMap<>();
 
-        if (filters.size () > 0) {
-            for (Map.Entry<String, String> filterEntry : filters.entrySet ()) {
-                String attributeKey = filterEntry.getKey ();
-                String filterValue = filterEntry.getValue ();
+        if (filters.size() > 0) {
+            for (Map.Entry<String, String> filterEntry : filters.entrySet()) {
+                String attributeKey = filterEntry.getKey();
+                String filterValue = filterEntry.getValue();
 
-                List<String> filterSplit = Arrays.asList ( filterValue.split ( ":" ) );
-                if (filterSplit.size () != 2) {
+                List<String> filterSplit = Arrays.asList(filterValue.split(":"));
+                if (filterSplit.size() != 2) {
                     continue;
                 }
 
-                List<Attribute> searchResults = new ArrayList<> ();
-                if (filterSplit.get ( 0 ).equals ( "eqc" )) {
+                List<Attribute> searchResults = new ArrayList<>();
+                if (filterSplit.get(0).equals("eqc")) {
                     // this means we're comparing strings
-                    searchResults = attributeRepository.findByProviderAndKeyAndValueContainingIgnoreCase ( gottenProvider,
-                            attributeKey, filterSplit.get ( 1 ) );
+                    searchResults = attributeRepository.findByProviderAndKeyAndValueContainingIgnoreCase(gottenProvider,
+                            attributeKey, filterSplit.get(1));
                 }
 
-                switch (filterSplit.get ( 0 )) {
+                switch (filterSplit.get(0)) {
                     case "eq":
-                        searchResults = attributeRepository.findByProviderAndKeyAndNumericValueEquals ( gottenProvider,
-                                attributeKey, Integer.parseInt ( filterSplit.get ( 1 ) ) );
+                        searchResults = attributeRepository.findByProviderAndKeyAndNumericValueEquals(gottenProvider,
+                                attributeKey, Integer.parseInt(filterSplit.get(1)));
                         break;
                     case "gt":
-                        searchResults = attributeRepository.findByProviderAndKeyAndNumericValueGreaterThan (
-                                gottenProvider, attributeKey, Integer.parseInt ( filterSplit.get ( 1 ) ) );
+                        searchResults = attributeRepository.findByProviderAndKeyAndNumericValueGreaterThan(
+                                gottenProvider, attributeKey, Integer.parseInt(filterSplit.get(1)));
                         break;
                     case "lt":
-                        searchResults = attributeRepository.findByProviderAndKeyAndNumericValueLessThan ( gottenProvider,
-                                attributeKey, Integer.parseInt ( filterSplit.get ( 1 ) ) );
+                        searchResults = attributeRepository.findByProviderAndKeyAndNumericValueLessThan(gottenProvider,
+                                attributeKey, Integer.parseInt(filterSplit.get(1)));
                         break;
                 }
 
                 for (Attribute attribute : searchResults) {
-                    Entry entry = attribute.getEntry ();
-                    entriesMap.put ( entry.getId (), entry );
+                    Entry entry = attribute.getEntry();
+                    entriesMap.put(entry.getId(), entry);
                 }
             }
 
         }
 
-        for (Map.Entry<Long, Entry> entryMapSet : entriesMap.entrySet ()) {
-            results.add ( Entry.toMap ( entryMapSet.getValue () ) );
+        for (Map.Entry<Long, Entry> entryMapSet : entriesMap.entrySet()) {
+            results.add(Entry.toMap(entryMapSet.getValue()));
         }
 
         return results;
